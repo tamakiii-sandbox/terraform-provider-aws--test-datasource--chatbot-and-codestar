10655773791b1b995148cfb601487ccd3eff72a0
tamakiii <tamakiii@users.noreply.github.com>
2025-05-23 23:28:40 +0900

    createiam role for codepipeline

--- main.tf
+++ main.tf
@@ -2,4 +2,35 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-data "aws_caller_identity" "current" {}
+variable "identifier" {
+  type    = string
+  default = "TestDatasourceChatbotAndCodestar"
+}
+
+variable "s3_codepipeline_artifact_arn" {
+  type = string
+}
+
+data "aws_s3_bucket" "codepipeline_artifact" {
+  bucket = var.s3_codepipeline_artifact_arn
+}
+
+resource "aws_iam_role" "codepipeline" {
+  name               = "${var.identifier}-CodePipeline"
+  assume_role_policy = data.aws_iam_policy_document.codepipeline_assume_role_policy.json
+
+  tags = {
+    Name = var.identifier
+  }
+}
+
+data "aws_iam_policy_document" "codepipeline_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+  }
+}
