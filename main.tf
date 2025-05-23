36d1e4f9eae7774e4a043c8fa1780fa485072194
tamakiii <tamakiii@users.noreply.github.com>
2025-05-23 23:39:43 +0900

    add policies to CodePipeline IAM role

--- main.tf
+++ main.tf
@@ -34,3 +34,38 @@ data "aws_iam_policy_document" "codepipeline_assume_role_policy" {
     }
   }
 }
+
+data "aws_iam_policy_document" "codepipeline" {
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning",
+      "s3:PutObject",
+    ]
+    resources = [
+      "${data.aws_s3_bucket.codepipeline_artifact.arn}",
+      "${data.aws_s3_bucket.codepipeline_artifact.arn}/*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "codebuild:BatchGetBuilds",
+      "codebuild:StartBuild"
+    ]
+    resources = [
+      "*" // TODO: fix me
+    ]
+  }
+}
+
+resource "aws_iam_policy" "codepipeline" {
+  name        = "${var.identifier}-CodePipeline"
+  policy      = data.aws_iam_policy_document.codepipeline.json
+}
+
+resource "aws_iam_role_policy_attachment" "codepipeline" {
+  role       = aws_iam_role.codepipeline.name
+  policy_arn = aws_iam_policy.codepipeline.arn
+}
