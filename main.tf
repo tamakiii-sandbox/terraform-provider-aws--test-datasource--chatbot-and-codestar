99dbb3bc14550205833de03b14fb7a92560c4d95
tamakiii <tamakiii@users.noreply.github.com>
2025-05-24 11:29:52 +0900

    add :*

--- main.tf
+++ main.tf
@@ -144,7 +144,8 @@ data "aws_iam_policy_document" "codebuild" {
       "logs:PutLogEvents"
     ]
     resources = [
-      aws_cloudwatch_log_group.codebuild_build.arn
+      "${aws_cloudwatch_log_group.codebuild_build.arn}",
+      "${aws_cloudwatch_log_group.codebuild_build.arn}:*",
     ]
   }
 
